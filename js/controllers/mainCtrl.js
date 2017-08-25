c6846c366c6220c9e8a99dbb82e1a2edbf7efbbb
Tyler <tyler@effenads.com>
2017-08-25 14:04:13 -0600

    files n stuff

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,4 +1,16 @@
 angular.module("jobAdApp").controller("mainCtrl", function ($scope) {
 
+    $scope.userClicked = false;
+
+    $scope.enlargeImg = function (fileName) {
+        $scope.userClicked = false;
+        $("#fw-container").addClass("full-width-img");
+        $scope.imgName = fileName;
+    }
+
+    $scope.closeImg = function () {
+        $("#fw-container").removeClass("full-width-img");
+        $scope.userClicked = true;
+    }
 
 });
